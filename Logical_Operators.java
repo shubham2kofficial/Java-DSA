ad50d40fe0cde9bb8fbc259a54a101e2414ffb47
shubham2k.official <shubham2k.official@gmail.com>
2024-05-24 15:22:44 +0530

    Create Logical_Operators.java

---
+++ Logical_Operators.java
@@ -0,0 +1,15 @@
+public class Logical_Operators {
+
+    public static void main(String[] args) {
+
+        // - Logical Operators
+
+        System.out.println((10 < 15) && (10 >= 5)); // - Logical AND
+
+        System.out.println((100 < 15) || (100 >= 50)); // - Logical OR
+
+        System.out.println(!(3 > 2)); // - Logical NOT
+
+    }
+
+}
