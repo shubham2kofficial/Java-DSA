f2f923cf1f6c9effa6a3cdf131019019cabe95cc
shubham2k.official <shubham2k.official@gmail.com>
2024-05-24 16:15:31 +0530

    Create While_loops.java

---
+++ While_loops.java
@@ -0,0 +1,19 @@
+public class While_loops {
+
+    public static void main(String[] args) {
+         
+        // - While Loops
+
+        int counter = 0;
+
+        while(counter < 100){
+        System.out.println("Hellow wrld");
+
+        counter ++;
+        }
+
+        System.out.println("hellow World 100x times");
+
+    }
+    
+}
