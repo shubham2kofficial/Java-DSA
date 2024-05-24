15246f11f35db513a022aa1415c93fcd40ad272d
shubham2k.official <shubham2k.official@gmail.com>
2024-05-24 15:26:17 +0530

    Create Product_of_AandB.java

---
+++ Product_of_AandB.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Product_of_AandB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        int b = sc.nextInt();
+
+        System.out.println("produt of a and b :" + a * b);
+
+    }
+}
