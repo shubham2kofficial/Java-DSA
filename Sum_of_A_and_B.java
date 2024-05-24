22a474e9ff60c3ab1c28106fb692cd12b3a23f20
shubham2k.official <shubham2k.official@gmail.com>
2024-05-24 15:07:28 +0530

    Create Sum_of_A_and_B.java

---
+++ Sum_of_A_and_B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+/**
+ * Sum_of_A_and_B
+ */
+public class Sum_of_A_and_B {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+    
+        System.out.println(a+b);
+    }
+}
