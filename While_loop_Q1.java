0bb21850cf53f2a245f66cb63c2a11ec23aaa821
shubham2k.official <shubham2k.official@gmail.com>
2024-05-24 16:22:17 +0530

    Print Numbers from 1 to 10 Using while loops

---
+++ While_loop_Q1.java
@@ -0,0 +1,15 @@
+public class While_loop_Q1 {
+
+    public static void main(String[] args) {
+
+        // - Print Numbers from 1 to 10 Using while loops
+
+        int counter = 1;
+
+        while (counter <= 10) {
+            System.out.println(counter);
+            counter++;
+        }
+    }
+
+}
