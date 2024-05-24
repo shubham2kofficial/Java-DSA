4c49a6fc21d38163c0468a9156093364aed7a992
shubham2k.official <shubham2k.official@gmail.com>
2024-05-24 16:45:45 +0530

    Do While loop

---
+++ Do_while_loop.java
@@ -0,0 +1,15 @@
+public class Do_while_loop {
+
+    public static void main(String[] args) {
+
+        // - Do While loop
+
+        int counter = 1;
+
+        do {
+            System.out.println("hello world");
+            counter++;
+        } while (counter <= 10);
+    }
+
+}
