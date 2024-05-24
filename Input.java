80cc32bc6c7e300157819a379049a6ef5958057f
shubham2k.official <shubham2k.official@gmail.com>
2024-05-24 15:03:47 +0530

    Create Input.java

---
+++ Input.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+/**
+ * Input
+ */
+public class Input {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // String input = sc.next();
+        
+        String input = sc.nextLine();
+        
+
+        System.out.println(input);
+    }
+}
