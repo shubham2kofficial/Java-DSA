eab4fa9bfcfb3bf52b52cc3eca05541f7db1ad0b
shubham2k.official <shubham2k.official@gmail.com>
2024-05-24 15:24:46 +0530

    Create Assignment_Operators.java

---
+++ Assignment_Operators.java
@@ -0,0 +1,45 @@
+public class Assignment_Operators {
+
+    public static void main(String[] args) {
+       
+       
+        // - Assignment Operator
+
+        int A = 20;
+
+        // A = A + 30;
+
+        A += 30;
+
+        System.out.println(A);
+
+        int B = 10;
+
+        // B = B - 5;
+        B -= 5;
+
+        System.out.println(B);
+
+        int C = 5;
+
+        // C = C * 5;
+        C *= 5;
+        System.out.println(C);
+
+        int D = 10;
+
+        // D = D / 2;
+        D /= 2;
+
+        System.out.println(D);
+
+        int F = 10;
+
+        // F = F % 2;
+        F %= 2;
+        System.out.println(F);
+
+        
+    }
+
+}
