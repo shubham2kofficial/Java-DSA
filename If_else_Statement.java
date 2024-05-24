2988375a5b2a01eaecc691e340c65ceeeb19f21d
shubham2k.official <shubham2k.official@gmail.com>
2024-05-24 15:30:56 +0530

    Create If_else_Statement.java

---
+++ If_else_Statement.java
@@ -0,0 +1,21 @@
+public class If_else_Statement {
+
+    public static void main(String[] args) {
+        
+        // - if-else Statement
+
+        int age = 22;
+
+        if(age >= 18){
+            System.out.println("Adult : Vote, Drive");
+        }
+        if(age>=13 && age<18){
+            System.out.println("teenagers");
+        }
+        else{
+            System.out.println("Not Adult");
+        }
+
+    }
+    
+}
