742784236544e99d70ef8a93fabf127eb3415919
shubham2k.official <shubham2k.official@gmail.com>
2024-05-24 17:00:21 +0530

    Continue Statement

---
+++ Continue_Statement.java
@@ -0,0 +1,19 @@
+
+public class Continue_Statement {
+
+    public static void main(String[] args) {
+
+        // - Continue Statement
+
+        for (int i = 1; i <= 5; i++) {
+
+            if (i == 3) {
+                continue;
+            }
+
+            System.out.println(i);
+        }
+        System.out.println();
+    }
+
+}
