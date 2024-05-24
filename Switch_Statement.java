253c71a2a2424f1438e9d40073027cfcd85831c0
shubham2k.official <shubham2k.official@gmail.com>
2024-05-24 16:09:12 +0530

    Create Switch_Statement.java

---
+++ Switch_Statement.java
@@ -0,0 +1,23 @@
+public class Switch_Statement {
+
+    public static void main(String[] args) {
+ 
+ 
+        // - Switch Statement
+
+        int number = 3;
+
+        switch (number) {
+           case 1 : System.out.println("Samosa");
+                  break;
+           case 2 : System.out.println("burgur");
+                  break;
+           case 3 : System.out.println("mango shake");
+                  break;
+           default : System.out.println("We wake Up");
+        }
+        
+
+    }
+    
+}
