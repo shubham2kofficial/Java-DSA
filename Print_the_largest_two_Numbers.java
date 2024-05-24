e2fd1559714442904fd61cc34caee68489510ac8
shubham2k.official <shubham2k.official@gmail.com>
2024-05-24 15:33:52 +0530

    Create Print_the_largest_two_Numbers.java

---
+++ Print_the_largest_two_Numbers.java
@@ -0,0 +1,16 @@
+public class Print_the_largest_two_Numbers {
+    public static void main(String[] args) {
+        
+        // - Print the largest two Numbers
+
+        int A = 40;
+        int B = 38;
+
+        if( A>=B ){
+            System.out.println("A is largest Number of 2");
+        }
+        else{
+            System.out.println("B is largest Number of 2");
+        }
+    }
+}
