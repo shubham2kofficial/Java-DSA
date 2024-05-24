606176ca7ba077a8d5032217397e0915b7bfc26c
shubham2k.official <shubham2k.official@gmail.com>
2024-05-24 15:19:47 +0530

    Create Relational_Operators.java

---
+++ Relational_Operators.java
@@ -0,0 +1,18 @@
+public class Relational_Operators {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 11;
+
+        System.out.println((a==b));
+
+        System.out.println((a != b ));
+
+        System.out.println((a > b)); // greator then
+        System.out.println((a < b)); // less then
+
+        System.out.println((a >= b));
+        System.out.println((a <= b));
+
+    }
+}
