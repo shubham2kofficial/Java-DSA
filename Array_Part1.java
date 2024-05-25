5b6ad6f980f97ac7481c93fe1e65493abec7b33a
shubham2k.official <shubham2k.official@gmail.com>
2024-05-25 11:02:12 +0530

    Array Cration

---
+++ Array_Part1.java
@@ -0,0 +1,16 @@
+public class Array_Part1 {
+
+    public static void main(String[] args) {
+
+        // // - Array Cration
+        
+        
+        int marks[] = new int[50];
+
+        int marks2[] = { 1, 2, 3, 4 };
+
+        String fruits[] = { "Apple", "Mango", "Banana", "Orange" };
+
+    }
+
+}
