ddc14e080213e1a7b94847f13d57d6db531ae04c
shubham2k.official <shubham2k.official@gmail.com>
2024-05-25 09:41:08 +0530

    Create Area_ofCircle.java

---
+++ Area_ofCircle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Area_ofCircle {
+
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+
+      float red = sc.nextFloat();
+      
+      float area = 3.14f * red * red ;
+
+      System.out.println(area);
+    }
+    
+}
