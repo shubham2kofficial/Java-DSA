b37224b280389fb4195393345ee1d96cdef6f60c
shubham2k.official <shubham2k.official@gmail.com>
2024-05-25 10:58:15 +0530

    hollow_Rhombus_Pattern

---
+++ Patterns_Part2_Advanced_Q8.java
@@ -0,0 +1,26 @@
+public class Patterns_Part2_Advanced_Q8 {
+
+    public static void hollow_Rhombus_Pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        hollow_Rhombus_Pattern(5);
+
+    }
+
+}
