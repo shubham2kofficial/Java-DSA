19c2bb347c637ca8a3002ec046c41f41cb20ac01
shubham2k.official <shubham2k.official@gmail.com>
2024-05-25 11:12:30 +0530

    Linear Search

---
+++ Array_Part1_Q1.java
@@ -0,0 +1,33 @@
+public class Array_Part1_Q1 {
+
+    // - Linear Search
+    public static int linearSearch(int numbers[], int key) {
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == key) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        // // - Linear Search
+
+        int numbers[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+
+        int key = 10;
+
+        int index = linearSearch(numbers, key);
+
+        if (index == -1) {
+            System.out.println("Not Found");
+        } else {
+            System.out.println("Key is at index : " + index);
+        }
+
+    }
+
+}
