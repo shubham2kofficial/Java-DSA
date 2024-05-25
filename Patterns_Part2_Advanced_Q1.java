7a29a8fca06198845c826f5d199bffec2ceaecf8
shubham2k.official <shubham2k.official@gmail.com>
2024-05-25 10:41:52 +0530

    hollow_Rectangle

---
+++ Patterns_Part2_Advanced_Q1.java
@@ -0,0 +1,24 @@
+public class Patterns_Part2_Advanced_Q1 {
+
+    public static void hollow_Rectangle(int totRow, int totCol) {
+
+        for (int i = 1; i <= totRow; i++) {
+            for (int j = 1; j <= totCol; j++) {
+                if (i == 1 || i == totRow || j == 1 || j == totCol) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        hollow_Rectangle(4, 5);
+
+    }
+
+}
