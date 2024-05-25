38bf99da3b21cb38099c2b3f9d0c9a6a2f4d20b7
shubham2k.official <shubham2k.official@gmail.com>
2024-05-25 10:55:36 +0530

    butterfly_Pattern

---
+++ Patterns_Part2_Advanced_Q6.java
@@ -0,0 +1,38 @@
+public class Patterns_Part2_Advanced_Q6 {
+
+    public static void butterfly_Pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        butterfly_Pattern(5);
+
+    }
+
+}
