05f20fd697ae6fa27dafdb11a31bc2af18f90190
shubham2k.official <shubham2k.official@gmail.com>
2024-05-25 10:54:08 +0530

    zeroOne_trianglePattern

---
+++ Patterns_Part2_Advanced_Q5.java
@@ -0,0 +1,21 @@
+public class Patterns_Part2_Advanced_Q5 {
+
+    public static void zeroOne_trianglePattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        zeroOne_trianglePattern(10);
+    }
+
+}
