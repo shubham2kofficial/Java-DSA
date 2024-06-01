9b2c6ba2d9f3694af92f737b35735bd7767e4661
shubham2k.official <shubham2k.official@gmail.com>
2024-06-01 11:02:44 +0530

    Counting Sort

---
+++ Sorting_Algorithems_CountingSort.java
@@ -0,0 +1,45 @@
+public class Sorting_Algorithems_CountingSort {
+
+    public static void countingSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+        int count[] = new int[largest + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        // Sorting
+
+        int j = 0;
+
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+
+    }
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {10, 3, 2, 4, 6, 5, 8, 7, 9, 1, 1, 4, 4, 9, 9, 9};
+
+        countingSort(arr);
+        printArray(arr);
+
+    }
+
+}
