0462a5d16ebc3a7565752a93126af300a1a4d1e7
shubham2k.official <shubham2k.official@gmail.com>
2024-06-06 08:31:26 +0530

    palindrome

---
+++ Strings/Palindrome.java
@@ -0,0 +1,28 @@
+package Strings;
+
+public class Palindrome {
+
+    public static boolean checkPalindrome(String str){
+
+        for(int i=0; i<str.length()/2; i++){
+            int n = str.length();
+            if(str.charAt(i) != str.charAt(n-1-i)){
+
+                return false;
+                
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        
+        String str = "racecar";
+
+        System.out.println(checkPalindrome(str));
+
+
+    }
+    
+}
---
+++ Strings/Strings.java
@@ -0,0 +1,47 @@
+package Strings;
+import java.util.*;
+
+public class Strings {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner (System.in);
+
+        char arr[] = {'a', 'b', 'c', 'd'};
+
+        // String str = "shubham Kumar";
+
+        // String str2 = new String("xyz");
+
+        // String name;
+
+        // //name = sc.next(); 
+
+        // name = sc.nextLine();
+
+        // System.out.println(name);
+        
+        // -> String Length
+        // String name = "Tony Stark";
+
+        // System.out.println(name.length());
+
+        // String str = "Hello, World!";
+        // char ch = str.charAt(0); // Accesses the character at index 0
+        // System.out.println("Character at index 0: " + ch);
+
+        
+        //Concatenation - Concatenation is the process of joining two or more strings end-to-end to form a new string.
+
+        String firtName = "Shubham Kumar";
+        String lastName = "Verma";
+
+        String fullName = firtName + " " + lastName;
+        System.out.println(fullName);
+
+        
+        
+
+    }
+    
+}
