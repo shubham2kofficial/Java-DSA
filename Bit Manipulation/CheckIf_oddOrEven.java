81f0e97f757b475fd9fd92ce6963d6a34fa0bfb7
shubham2k.official <shubham2k.official@gmail.com>
2024-06-12 08:51:00 +0530

    CheckIf_oddOrEven.java

---
+++ Bit Manipulation/CheckIf_oddOrEven.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class CheckIf_oddOrEven {
+
+    public static void oddOrEven(int n){
+        int bitmask =1;
+
+       if((n & bitmask) == 0){
+        System.out.println("Even Number");
+       }
+       else{
+         System.out.println("odd Nuber");
+       }
+    }
+
+    public static void main(String[] args) {
+
+        oddOrEven(5);
+        oddOrEven(10);
+        oddOrEven(15);
+        
+        
+    }
+    
+}
