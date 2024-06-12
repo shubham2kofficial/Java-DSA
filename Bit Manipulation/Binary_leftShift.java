884c375d7918ef26df818b1d7df167967653eb5f
shubham2k.official <shubham2k.official@gmail.com>
2024-06-12 08:21:58 +0530

    Binary Left Shift & right Shift

--- Bit Manipulation/Binary_leftShift.java
+++ Bit Manipulation/Binary_leftShift.java
@@ -16,19 +16,7 @@ public class Binary_leftShift {
          // Print the number obtained after shifting x by n positions towards left
          System.out.println("Left shift " + x + " by " + n + " positions : " + answer);
           
-         // Number to be shifted
-         x = answer;
-            
-         // Number of positions 
-         n = 2;
-          
-         // Shifting x by n positions towards left using left shift operator
-         answer = answer << n;
-            
-         // Print the number obtained after shifting x by n positions towards left
-         System.out.println("Left shift " + x + " by " + n + " positions : " + answer);
-
-         // Formula : Left shift x by n positions A*2(pow)B
+           // Formula : Left shift x by n positions A*2(pow)B
 
          
           
---
+++ Bit Manipulation/Binary_rightShift.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Binary_rightShift {
+
+    public static void main(String[] args) {
+       // Number to be shifted
+       int x = 6;
+           
+       // Number of positions 
+       int n = 1;
+        
+       // Shifting x by n positions towards Right using Right shift operator
+       int answer = x >> n;
+        
+       // Print the number obtained after shifting x by n positions towards Right
+       System.out.println("Right shift " + x + " by " + n + " positions : " + answer);
+        
+      
+
+       // Formula : Left shift x by n positions A/2(pow)B
+
+    }
+    
+}
